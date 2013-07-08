0de8596ad7ea0d3db2841c6200bcc31725d4b394
David Mosher <davidmosher@gmail.com>
2013-07-07 22:34:31 -0600

    Adds sourcemap generation with inlined sources

--- config/application.js
+++ config/application.js
@@ -23,4 +23,34 @@ module.exports = require(process.env['LINEMAN_MAIN']).config.extend('application
   //   }
   // }
 
+  loadNpmTasks: [
+    "grunt-concat-sourcemap"
+  ],
+
+  removeTasks: {
+    common: ["concat"]
+  },
+
+  appendTasks: {
+    common: ["concat_sourcemap"]
+  },
+
+  concat_sourcemap: {
+    options: {
+      sourcesContent: true
+    },
+    js: {
+      src: ["<banner:meta.banner>", "<%= files.js.vendor %>", "<%= files.template.generated %>", "<%= files.coffee.generated %>", "<%= files.js.app %>"],
+      dest: "<%= files.js.concatenated %>"
+    },
+    spec: {
+      src: ["<%= files.js.specHelpers %>", "<%= files.coffee.generatedSpecHelpers %>", "<%= files.js.spec %>", "<%= files.coffee.generatedSpec %>"],
+      dest: "<%= files.js.concatenatedSpec %>"
+    },
+    css: {
+      src: ["<%= files.less.generatedVendor %>", "<%= files.sass.generatedVendor %>", "<%= files.css.vendor %>", "<%= files.less.generatedApp %>", "<%= files.sass.generatedApp %>", "<%= files.css.app %>"],
+      dest: "<%= files.css.concatenated %>"
+    }
+  }
+
 });
