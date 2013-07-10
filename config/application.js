cf92b6497f7de2d3518f63694c5dbd3efc02f5a8
David Mosher <davidmosher@gmail.com>
2013-07-10 10:24:40 -0600

    Updates watch config to handle concat_sourcemap

--- config/application.js
+++ config/application.js
@@ -51,6 +51,37 @@ module.exports = require(process.env['LINEMAN_MAIN']).config.extend('application
       src: ["<%= files.less.generatedVendor %>", "<%= files.sass.generatedVendor %>", "<%= files.css.vendor %>", "<%= files.less.generatedApp %>", "<%= files.sass.generatedApp %>", "<%= files.css.app %>"],
       dest: "<%= files.css.concatenated %>"
     }
+  },
+
+  watch: {
+    js: {
+      files: ["<%= files.js.vendor %>", "<%= files.js.app %>"],
+      tasks: ["concat_sourcemap:js"]
+    },
+    coffee: {
+      files: "<%= files.coffee.app %>",
+      tasks: ["coffee", "concat_sourcemap:js"]
+    },
+    jsSpecs: {
+      files: ["<%= files.js.specHelpers %>", "<%= files.js.spec %>"],
+      tasks: ["concat_sourcemap:spec"]
+    },
+    coffeeSpecs: {
+      files: ["<%= files.coffee.specHelpers %>", "<%= files.coffee.spec %>"],
+      tasks: ["coffee", "concat_sourcemap:spec"]
+    },
+    css: {
+      files: ["<%= files.css.vendor %>", "<%= files.css.app %>"],
+      tasks: ["concat_sourcemap:css"]
+    },
+    less: {
+      files: ["<%= files.less.vendor %>", "<%= files.less.app %>"],
+      tasks: ["less", "concat_sourcemap:css"]
+    },
+    sass: {
+      files: ["<%= files.sass.vendor %>", "<%= files.sass.app %>"],
+      tasks: ["sass", "concat_sourcemap:css"]
+    }
   }
 
 });
